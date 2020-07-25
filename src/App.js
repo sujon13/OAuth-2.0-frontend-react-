712b2f0e86b632fbf649bf02cc28dfb4ea273bce
sujon13 <arifurrahmansujon27@gmail.com>
2020-07-25 21:25:54 +0600

    Created frontend part for oauth 2.0 demo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const login = async () => {
+        const config = {
+            url: 'http://127.0.0.1:4000/api/v1/oauth/login'
+        };
+        try {
+            const permissionPage = await Axios.request(config);
+            console.log(permissionPage.data);
+        } catch (error) {
+            console.log('Error!');
+            console.log(error);
+        }
+    };
+    return (
+        <div className="App">
+            <a href="https://github.com/login/oauth/authorize?client_id=7d95f74c54996b80ce9b&redirect_uri=http://127.0.0.1:4000/api/v1/oauth/redirect&scope=user">
+                click here to get your github profile
+            </a>
+        </div>
+    );
+};
 
 export default App;
